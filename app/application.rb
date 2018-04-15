a2025b5f89be421926fe5a782f8a3dd14efd7e93
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-15 00:37:00 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+
+class Application
+
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+end
+    if req.path.match("/items/")
+      if item =@@items.find {|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status=404
+      resp.write "Route not found"
+    end
+   resp.finish
+  end
+end
--- app/item.rb
+++ app/item.rb
@@ -1,23 +1,8 @@
-class Application
-  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+class Item
+attr_accessor :name
+attr_accessor :price
 
-def call(env)
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
-
- if req.path.match(/items/)
-   item_name = req.path.split("/items/").last
-   if item =@@items.find{|i| i.name == item_name}
-   resp.write item.price
- else
-   resp.status = 400
-   resp.write "Item not found"
+def initialize(name, price)
+  @name = name
+  @price = price
 end
- else
-   resp.status = 404
-   resp.write "Route not found"
-
- end
- resp.finish
- end
- end
